2edef1b524e033835375aeb025c56f7327a7c574
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-06-22 23:26:58 +0530

    validation added

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,8 @@
 class Friend < ApplicationRecord
     belongs_to :user
+    validates :first_name, presence: { message: "First name is required" }
+    validates :last_name, presence: { message: "Last name is required" }
+    validates :email, presence: { message: "Email is required" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid ( example@gmail.com )" }
+    validates :phone, presence: { message: "Phone number is required" }, format: { with: /\A[0-9]{10}\z/, message: "Phone number should only contain numbers" }, length: { is: 10, message: "Phone number should be exactly 10 digits" }
+    validates :twitter, presence: { message: "Twitter name is required" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,38 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          has_many :friends      
+          has_many :friends     
+          
+          #validates :email, presence: { message: "Email is required" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid ( example@gmail.com )" }
+          #validates :password, presence: { message: "Password is required" }    
+          
+          validate :password_match_validation
+
+          def password_match_validation
+
+            if password.blank?
+              errors.add(:password, "Password is required")
+            end  
+
+            if password.present? && password_confirmation.present? && password != password_confirmation
+              errors.add(:password_confirmation, "doesn't match Password")
+            end
+        
+            if password.present?
+              unless password.match?(/^(?=.*[A-Z])(?=.*\d)(?=.*[[:^alnum:]])[A-Za-z\d[:^alnum:]]{6,}$/)
+                errors.add(:password, "must be at least 6 characters long and include at least one symbol, one number, and one uppercase letter")
+              end
+            end
+          end
+
+          validate :email_format_validation
+
+          def email_format_validation
+            if email.blank?
+              errors.add(:email, "Email is required")
+            elsif !email.match?(URI::MailTo::EMAIL_REGEXP)
+              errors.add(:email, "is not in a valid format")
+            end
+          end  
+
 end
